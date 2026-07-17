6791a8fb305da2118861e911118d877dd53c3f60
leonel-oficialDEV <leonelomana57@gmail.com>
2026-07-17 00:21:30 -0400

    Actualizar database.js

--- core/system/database.js
+++ core/system/database.js
@@ -115,15 +115,15 @@ export const defSets = {
   self: 0,
   prefix: '[\"/\",\"!\",\".\",\"#\"]',
   commandsejecut: 0,
-  newsletter_id: '120363401404146384@newsletter',
-  nameid: 'ೃ࿔ ყµҡเ ωαɓσƭร - σƒƒเ૮เαℓ ૮ɦαɳɳεℓ .ೃ࿐',
+  newsletter_id: '120363324350463849@newsletter',
+  nameid: '❤️sumi sakurasawa❤️',
   type: 'Owner',
-  link: 'https://api.yuki-wabot.my.id',
-  banner: 'https://cdn.yuki-wabot.my.id/files/tCVD.jpeg',
-  icon: 'https://cdn.yuki-wabot.my.id/files/4U5V.jpeg',
+  link: 'leonel',
+  banner: 'https://cdn.yuki-wabot.my.id/files/6VvE.jpeg',
+  icon: 'https://cdn.yuki-wabot.my.id/files/6VvE.jpeg',
   currency: 'Yenes',
-  namebot: 'Yuki',
-  botname: 'Yuki Suou',
+  namebot: '❤️SUMI BOT❤️',
+  botname: 'sumi sakurasawa',
   owner: ''
 };
 
@@ -188,15 +188,15 @@ export function initDB() {
       self BOOLEAN DEFAULT 0,
       prefix TEXT DEFAULT '[\"/\",\"!\",\".\",\"#\"]',
       commandsejecut INTEGER DEFAULT 0,
-      newsletter_id TEXT DEFAULT '120363401404146384@newsletter',
-      nameid TEXT DEFAULT 'ೃ࿔ ყµҡเ ωαɓσƭร - σƒƒเ૮เαℓ ૮ɦαɳɳεℓ .ೃ࿐',
+      newsletter_id TEXT DEFAULT '120363324350463849@newsletter',
+      nameid TEXT DEFAULT '❤️sumi sakurasawa❤️',
       type TEXT DEFAULT 'Owner',
-      link TEXT DEFAULT 'https://api.yuki-wabot.my.id',
-      banner TEXT DEFAULT 'https://cdn.yuki-wabot.my.id/files/tCVD.jpeg',
-      icon TEXT DEFAULT 'https://cdn.yuki-wabot.my.id/files/4U5V.jpeg',
+      link TEXT DEFAULT 'leonel',
+      banner TEXT DEFAULT 'https://cdn.yuki-wabot.my.id/files/6VvE.jpeg',
+      icon TEXT DEFAULT 'https://cdn.yuki-wabot.my.id/files/6VvE.jpeg',
       currency TEXT DEFAULT 'Yenes',
-      namebot TEXT DEFAULT 'Yuki',
-      botname TEXT DEFAULT 'Yuki Suou',
+      namebot TEXT DEFAULT 'sumi',
+      botname TEXT DEFAULT 'sumi bot',
       owner TEXT DEFAULT ''
     )`);
   db.exec(`CREATE TABLE IF NOT EXISTS characters (id TEXT PRIMARY KEY, data TEXT)`);
@@ -502,4 +502,5 @@ try {
       const defaultValue = table.def[col];
       let sqlType = 'TEXT';
       if (typeof defaultValue === 'number') sqlType = 'INTEGER';
-      else if (typ
+      else if (typeof defaultValue === 'boolean') sqlType = 'BOOLEAN';
+      const de
